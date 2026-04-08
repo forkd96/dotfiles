4f6bea24acbf0f1e7964b22be019bba759e53deb
forkd <forkd@tutamail.com>
2026-04-09 02:47:45 +0300

    update fish config

---
+++ fish/config.fish
@@ -0,0 +1,18 @@
+if status is-interactive
+
+fastfetch
+
+alias st="subtui"
+alias nt="nmtui"
+alias bt="bluetui"
+alias ls="eza --icons always"
+alias b="bat -l conf -p"
+alias nv="nvim"
+alias nano="nvim"
+alias il="impala"
+
+set -gx TERM xterm-256color
+
+set fish_greeting ''
+
+end
