6e58b335597e06deacdfe6bfab3475ac725300c0
forkd <forkd@tutamail.com>
2026-04-19 05:33:19 +0300

    add otter to flake.nix

--- flake.nix
+++ flake.nix
@@ -1,33 +1,55 @@
 {
-  description = "NixOS + Home Manager";
+  description = "stuff";
+
   inputs = {
+    # --- nixpkgs ---
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     nixpkgs-stable.url = "nixpkgs/nixos-25.11";
 
+    # --- HM --- 
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
+    # --- nixvim ---
     nixvim = {
       url = "github:nix-community/nixvim";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
+    # --- flake-parts for otter --- 
+    systems.url = "github:nix-systems/default-linux";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+
+    # --- otter-launcher --- 
+    otter-launcher = {
+      url = "github:kuokuo123/otter-launcher";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.home-manager.follows = "home-manager";
+      inputs.systems.follows = "systems";
+      inputs.flake-parts.follows = "flake-parts";
+    };
   };
 
-  outputs = { self, nixpkgs, home-manager, nixvim, ... }: {
+  outputs = { self, nixpkgs, home-manager, nixvim, otter-launcher, ... }: {
+
     nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       modules = [
         ./nixos/configuration.nix
         home-manager.nixosModules.home-manager
         {
-          home-manager.useGlobalPkgs = true;
-          home-manager.useUserPackages = true;
-          home-manager.sharedModules = [ nixvim.homeModules.nixvim ];
+          home-manager.useGlobalPkgs    = true;
+          home-manager.useUserPackages  = true;
+          home-manager.sharedModules    = [
+            nixvim.homeModules.nixvim
+            otter-launcher.homeModules.default
+          ];
           home-manager.users.forkd = import ./home-manager;
         }
       ];
     };
+
   };
 }
